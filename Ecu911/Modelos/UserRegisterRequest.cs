b666d49fc101b991d12fed47b454d626bff3a372
Sebastian99fuentes <80362481+Sebastian99fuentes@users.noreply.github.com>
2022-12-30 13:53:46 -0500

    Usuarios sf7

--- Ecu911/Modelos/UserRegisterRequest.cs
+++ Ecu911/Modelos/UserRegisterRequest.cs
@@ -4,15 +4,12 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Ecu911.Modelos
 {
-    public partial class UserRegisterRequest 
+    public partial class UserRegisterRequest
     {
         public string userName { get; set; }
+
         public string rol { get; set; }
-<<<<<<< HEAD:Ecu911/Modelos/Useregistrar.cs
-        public string plantaId { get; set; }
-=======
 
         public string planta { get; set; }
->>>>>>> b1926743a8abd4f9c534d940fecb47fee2ee5d67:Ecu911/Modelos/UserRegisterRequest.cs
     }
 }
